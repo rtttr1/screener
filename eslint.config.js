ca530a0d05c45911e32716a5f5eab18578f91304
rtttr1 <bobo3766@naver.com>
2025-12-02 01:28:25 +0900

    [#1] 🎨 @naverpay/eslint-config 적용

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import naverpay from "@naverpay/eslint-config";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    ignores: ["**/dist/**"],
   },
-])
+  ...naverpay.configs.node,
+  ...naverpay.configs.typescript,
+  ...naverpay.configs.react,
+  ...naverpay.configs.packageJson,
+];
