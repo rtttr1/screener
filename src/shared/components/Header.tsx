6d3218e446b292fd8c4b8468c622de6d54ec3166
rtttr1 <bobo3766@naver.com>
2025-12-02 18:19:47 +0900

    [#9] ✨ Header 구현

---
+++ src/shared/components/Header.tsx
@@ -0,0 +1,12 @@
+import SvgBadgeNpay from '../assets/svg/BadgeNpay'
+
+const Header = () => {
+    return (
+        <header className="flex items-center gap-2 px-8 py-6 border-b border-gray-200">
+            <SvgBadgeNpay width={100} />
+            <h1 className="text-xl font-bold">Stock Screener</h1>
+        </header>
+    )
+}
+
+export default Header
