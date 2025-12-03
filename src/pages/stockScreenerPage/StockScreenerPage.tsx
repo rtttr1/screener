225384cc4d489acb83bba18ac48d4cb77fb5bba0
rtttr1 <bobo3766@naver.com>
2025-12-03 14:50:10 +0900

    [#13] ✨ StockMarketSelectionSection 위치 결정

---
+++ src/pages/stockScreenerPage/StockScreenerPage.tsx
@@ -0,0 +1,13 @@
+import StockMarketSelectionSection from '@/pages/stockScreenerPage/components/StockMarketSelectionSection'
+
+const StockScreenerPage = () => {
+    return (
+        <main>
+            <StockMarketSelectionSection />
+            {/* Stock Screener Filter */}
+            {/* Stock Screener Table */}
+        </main>
+    )
+}
+
+export default StockScreenerPage
