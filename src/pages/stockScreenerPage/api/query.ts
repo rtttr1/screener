b04e3c09fc7f86bc2b6387adb02aead8cfe89a0a
rtttr1 <bobo3766@naver.com>
2025-12-04 23:39:33 +0900

    [#27] ✨ 해외 주식 리스트 쿼리 구현

--- src/pages/stockScreenerPage/api/query.ts
+++ src/pages/stockScreenerPage/api/query.ts
@@ -1,13 +1,21 @@
 import {useQuery} from '@tanstack/react-query'
 
-import type {GetDomesticStockListRequest} from '@/pages/stockScreenerPage/types/api'
+import type {GetDomesticStockListRequest, GetOverseasStockListRequest} from '@/pages/stockScreenerPage/types/api'
 
-import {getDomesticStockList} from '@/pages/stockScreenerPage/api/api'
+import {getDomesticStockList, getOverseasStockList} from '@/pages/stockScreenerPage/api/api'
 
 export const useDomesticStockList = (params: GetDomesticStockListRequest) => {
     return useQuery({
         queryKey: ['domesticStockList', params],
         queryFn: () => getDomesticStockList(params),
         enabled: !!params.sortType,
+    })
+}
+
+export const useOverseasStockList = (params: GetOverseasStockListRequest) => {
+    return useQuery({
+        queryKey: ['overseasStockList', params],
+        queryFn: () => getOverseasStockList(params),
+        enabled: !!params.stockExchangeType && !!params.sortType,
     })
 }
