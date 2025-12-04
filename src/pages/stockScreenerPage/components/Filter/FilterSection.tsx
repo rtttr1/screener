eef0d34f62f4168f3f058b074ce1bbf1a2d8118e
rtttr1 <bobo3766@naver.com>
2025-12-04 23:49:39 +0900

    [#27] ✨ 해외 탭일때 거래소 필터 랜더링 x

--- src/pages/stockScreenerPage/components/Filter/FilterSection.tsx
+++ src/pages/stockScreenerPage/components/Filter/FilterSection.tsx
@@ -1,19 +1,25 @@
 import {useAtom} from 'jotai'
+import {useSearchParams} from 'react-router-dom'
 
 import {hasActiveFiltersAtom} from '@/pages/stockScreenerPage/atoms/filterAtoms'
 import ExchangeFilter from '@/pages/stockScreenerPage/components/Filter/ExchangeFilter'
 import FilterResetAllButton from '@/pages/stockScreenerPage/components/Filter/FilterResetAllButton'
 import PriceChangeFilter from '@/pages/stockScreenerPage/components/Filter/PriceChangeFilter'
 import PriceChangeRateFilter from '@/pages/stockScreenerPage/components/Filter/PriceChangeRateFilter'
+import {REGIONS} from '@/pages/stockScreenerPage/constants/region'
+import {URL_QUERIES} from '@/pages/stockScreenerPage/constants/urlQueries'
 
 const FilterSection = () => {
     const [hasActiveFilters] = useAtom(hasActiveFiltersAtom)
+
+    const [searchParams] = useSearchParams()
+    const isDomestic = searchParams.get(URL_QUERIES.REGION) === REGIONS.DOMESTIC
 
     return (
         <section aria-label="스크리너 필터" className="flex items-center justify-end gap-2 p-4">
             <PriceChangeFilter />
             <PriceChangeRateFilter />
-            <ExchangeFilter />
+            {isDomestic && <ExchangeFilter />}
             {hasActiveFilters && <FilterResetAllButton />}
         </section>
     )
