e91ef4f65f25cd061b34b4225bf91674f625ec4e
rtttr1 <bobo3766@naver.com>
2025-12-07 15:57:10 +0900

    [#42] ✨ 실시간 주식 정보 조회 api 구현

--- src/pages/stockScreenerPage/api/api.ts
+++ src/pages/stockScreenerPage/api/api.ts
@@ -3,6 +3,8 @@ import type {
     GetDomesticStockListResponse,
     GetOverseasStockListRequest,
     GetOverseasStockListResponse,
+    GetRealTimeStockRequest,
+    GetRealTimeStockResponse,
     OverseasStockListApiResponse,
 } from '@/pages/stockScreenerPage/types/api'
 
@@ -35,4 +37,19 @@ export const getOverseasStockList = async (
         ...response,
         stocks: response.stocks.map(mapOverseasStockToStock),
     }
+}
+
+export const getRealTimeStock = async (params: GetRealTimeStockRequest): Promise<GetRealTimeStockResponse> => {
+    const searchParams = buildSearchParams({
+        type: params.type,
+    })
+
+    return mStockClient
+        .post('realTime/stock', {
+            searchParams,
+            json: {
+                reutersCodes: params.reutersCodes,
+            },
+        })
+        .json<GetRealTimeStockResponse>()
 }
