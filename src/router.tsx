c99a45ab3ee320f23c472407967e85a52103b5bd
rtttr1 <bobo3766@naver.com>
2025-12-09 01:03:01 +0900

    [#41] ✨ 404 에러핸들링

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from 'react-router-dom'
 
 import Layout from '@/Layout'
+import NotFoundPage from '@/pages/NotFoundPage'
 import StockScreenerPage from '@/pages/stockScreenerPage/StockScreenerPage'
 
 export const router = createBrowserRouter([
@@ -13,5 +14,9 @@ export const router = createBrowserRouter([
                 element: <StockScreenerPage />,
             },
         ],
+    },
+    {
+        path: '*',
+        element: <NotFoundPage />,
     },
 ])
