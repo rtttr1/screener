8dc072aa44755802a91616d5eafb463ef846b696
rtttr1 <bobo3766@naver.com>
2025-12-09 16:07:15 +0900

    [#41] ♻️ retry 다시 1로 설정

--- src/common/lib/queryClient.ts
+++ src/common/lib/queryClient.ts
@@ -4,7 +4,7 @@ export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             refetchOnWindowFocus: false,
-            retry: 0,
+            retry: 1,
             staleTime: 1000 * 60 * 5, // 5분
         },
     },
